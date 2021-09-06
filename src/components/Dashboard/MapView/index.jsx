82893f9d12bbead4d1d0ab6c6299168381eefd4e
Matheus Teles <matheustdo1@gmail.com>
2021-09-06 17:01:14 -0300

    Remove rotation from map

--- src/components/Dashboard/MapView/index.jsx
+++ src/components/Dashboard/MapView/index.jsx
@@ -52,6 +52,7 @@ export default function MapView({
         ref={mapRef}
         width="100%"
         height="100%"
+        dragRotate={false}
         mapStyle="mapbox://styles/infoamazonia/ckhe037kt07on1aql47yvp2rn"
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
